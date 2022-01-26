1c0aced6b3c420ad371ce7c8804902a456fc6766
Randy Girard <randy@tradepending.com>
2022-01-26 13:54:49 -0500

    Add more content flags

--- 220124065451_content_flag_zones.sql
+++ 220124065451_content_flag_zones.sql
@@ -1,5 +1,7 @@
-UPDATE zones SET content_flags = 'january_2000_sky_era' WHERE short_name = 'airplane';
-UPDATE zones SET content_flags = 'july_1999_fear_era' WHERE short_name = 'fearplane';
-UPDATE zones SET content_flags = 'october_1999_temple_era' WHERE short_name = 'hateplane';
-UPDATE zones SET content_flags = 'february_2000' WHERE short_name = 'paineel';
-UPDATE zones SET content_flags = 'october_1999_temple_era' WHERE short_name = 'soltemple';
+UPDATE zone SET content_flags = 'january_2000_sky_era' WHERE short_name = 'airplane';
+UPDATE zone SET content_flags = 'july_1999_fear_era' WHERE short_name = 'fearplane';
+UPDATE zone SET content_flags = 'october_1999_temple_era' WHERE short_name = 'hateplane';
+UPDATE zone SET content_flags = 'february_2000' WHERE short_name = 'paineel';
+UPDATE zone SET content_flags = 'october_1999_temple_era' WHERE short_name = 'soltemple';
+UPDATE zone SET content_flags = 'june_2000' WHERE short_name = 'hole';
+UPDATE zone SET content_flags = 'june_2000' WHERE short_name = 'veeshan';
