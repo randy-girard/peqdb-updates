2041f70d3536ee4144676b97028608a48e6a08f9
Randy Girard <randy@tradepending.com>
2022-01-26 13:55:47 -0500

    Cleanup fix

--- paw_prevamp/cleanup.sql
+++ paw_prevamp/cleanup.sql
@@ -1,3 +1,5 @@
+SET SQL_SAFE_UPDATES = 0;
+
 -- Add content flag to SK ishva mal spawn group entries
 UPDATE peq.spawnentry SET content_flags = 'january_2005_paw_revamp' where spawngroupId = 117731;
 
@@ -10,3 +12,5 @@ update npc_types set loottable_id = loottable_id + 300000 where id >= 18000 and 
 -- Allows for original paw
 UPDATE spawn2 SET content_flags = 'april_2000_splitpaw_revamp' WHERE content_flags_disabled = 'january_2005_paw_revamp' and zone = 'paw';
 UPDATE spawnentry SET content_flags = 'april_2000_splitpaw_revamp' WHERE content_flags_disabled = 'january_2005_paw_revamp' AND npcID >= 18000 AND npcID < 19000;
+
+SET SQL_SAFE_UPDATES = 1;
