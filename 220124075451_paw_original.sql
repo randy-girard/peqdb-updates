6a5cd7c2cbc38eb065f4a1c9206f0d0b11cd69e1
Randy Girard <randy@tradepending.com>
2022-01-26 16:00:43 -0500

    Fix script

--- 220124075451_paw_original.sql
+++ 220124075451_paw_original.sql
@@ -1,11 +1,9 @@
-source ./paw_original/init.sql
--- source ./paw_original/lootdrop_entries.sql
--- source ./paw_original/lootdrop.sql
--- source ./paw_original/loottable_entries.sql
--- source ./paw_original/loottable.sql
--- source ./paw_original/npc_faction.sql
+source ./paw_original/lootdrop_entries.sql
+source ./paw_original/lootdrop.sql
+source ./paw_original/loottable_entries.sql
+source ./paw_original/loottable.sql
+source ./paw_original/npc_faction.sql
 source ./paw_original/npc_types.sql
 source ./paw_original/spawn2.sql
 source ./paw_original/spawnentry.sql
 source ./paw_original/spawngroup.sql
-source ./paw_original/cleanup.sql
